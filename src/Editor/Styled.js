f32fda04dc9d67b60c362c7681354d02389fdce2
Nicolofranceschi <franceschinicolo@gmail.com>
2021-06-04 10:35:12 +0200

    Rebuild drawer

---
+++ src/Editor/Styled.js
@@ -0,0 +1,83 @@
+
+import styled from 'styled-components';
+import {motion} from "framer-motion";
+
+export const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  overflow: auto;
+  -ms-overflow-style: none;  /* IE and Edge */
+      scrollbar-width: none;  /* Firefox */
+      ::-webkit-scrollbar {
+        display: none;
+      }
+  `;
+
+export const Grid = styled.div`
+      background-color: var(--black-light);
+      height: ${props => props.gridSize}px;
+      width: ${props => props.gridSize}px;
+      display: grid;
+      grid-template-columns: repeat(70,${props => props.pixelSize}px);
+      grid-template-rows: repeat(70,${props => props.pixelSize}px);
+      
+`;
+
+export const Pixelstyle = styled.div`
+      border: 1px solid var(--line);
+      box-sizing: border-box;
+`;
+
+
+export const BottomPopup = styled(motion.div)`
+
+  width: ${props => props.width};
+  height: ${props => props.height};
+  background: white;
+  position: absolute;
+  border-radius: 20px 20px 0 0;
+  z-index: 50;
+  margin-top: ${({ marginBottom }) => marginBottom ? `${-marginBottom}px` : 0};
+  bottom: ${({ bottom }) => bottom ? `${bottom}px` : 0};
+  @media (min-width: 768px) {
+    border-radius: 20px;
+    top: 20px;
+    right: 20px;  
+  }
+`;
+
+export const Action = styled.div`
+
+     position: absolute;
+     top : 0;
+     right : 0;
+     width: 50px;
+     height: 50px;
+     background-color: white;
+     border-radius: 20px;
+`;
+
+export const Color = styled.div`
+
+     position: absolute;
+     top : 10%;
+     left : 5% ;
+     
+`;
+
+export const Svg = styled.svg`
+ 
+ position: absolute;
+    top : 10px;
+    left : 10px;
+    height: 30px;
+    width: 30px;
+    stroke: var(--medium-gray);
+    &:hover{
+      stroke: var(--black-light);
+    }
+
+`;
+
+
+
---
+++ src/Editor/Tools.js
@@ -0,0 +1,129 @@
+
+import { CirclePicker } from 'react-color'
+
+import { BottomPopup , Action, Color , Svg} from "./Styled"
+
+import { useWindowSize } from "../SIze.js";
+
+import { useState } from 'react';
+
+
+
+
+export function Tools (props) {
+
+    const {height,width} = useWindowSize();
+
+    const [closedPopupHeight, setClosedPopupHeight]= useState(50);
+
+    const [{H,W,B}, setAnimation ] = useState (
+        {
+            H: "50px",
+            W: "50px",
+            B:0
+
+        }
+    );
+  
+    const openPopupHeight = - height + closedPopupHeight;
+
+    return  width >= 768 ? (
+
+   <>
+
+    { B==1 ? ( 
+
+    <BottomPopup
+
+            drag
+            dragConstraints={{ top: 0, bottom: height-height/100*60-30 , left: -width+width/100*30+30  , right: 0 }}
+            dragElastic={0}
+            dragTransition={{ bounceStiffness: 1300, bounceDamping: 20 }}
+            width={W}
+            height={H}
+    
+          >
+
+           <Action onClick={ () =>
+
+                    setAnimation(
+
+                        {
+                            H: "50px",
+                            W: "50px",
+                            B:0
+
+                        }
+                    )
+                    }>
+
+            <Svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" >
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M18 12H6" />
+            </Svg>   
+            </Action>
+                <Color>
+                    <CirclePicker width={"100%"} onChangeComplete={(color) => {
+                        console.log(color.hex);
+                        props.setpixelcolor(color.hex);
+                    }}/>
+                </Color>
+    </BottomPopup>   
+):(
+    <BottomPopup
+
+            drag
+            dragConstraints={{ top: 0, bottom: height-80 , left: -width-80  , right: 0 }}
+            dragElastic={0}
+            dragTransition={{ bounceStiffness: 1300, bounceDamping: 20 }}
+            width={W}
+            height={H}
+    
+          >
+                    <Action onClick={ () =>
+
+                                setAnimation(
+
+                                    {
+                                        H: "60vh",
+                                        W: "30vw",
+                                        B:1
+    
+                                    }
+                                )
+                                }> 
+
+                    <Svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+                    </Svg> 
+                    </Action>
+                    
+        </BottomPopup>
+
+                )}
+
+      </> 
+
+    ):(
+
+        <BottomPopup
+
+        drag="y"
+        dragConstraints={{ top: openPopupHeight, bottom: 0 }}
+        dragElastic={0}
+        dragTransition={{ bounceStiffness: 1300, bounceDamping: 20 }}
+        bottom={openPopupHeight}
+        marginBottom={closedPopupHeight}
+        width={"100vw"}
+        height={"100vh"}
+
+      >
+           <Color>
+                    <CirclePicker width={"100%"} onChangeComplete={(color) => {
+                        console.log(color.hex);
+                        props.setpixelcolor(color.hex);
+                    }}/>
+                </Color>
+
+       </BottomPopup>
+    );
+}
