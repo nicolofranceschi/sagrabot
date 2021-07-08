68831ad0fea8dc3c3c3f889de9cb03dcecd0132f
Nicolofranceschi <franceschinicolo@gmail.com>
2021-07-08 12:56:56 +0200

    login

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,45 @@
-import { createContext, useContext } from 'react';
+import { createContext, useContext , useState, useEffect } from 'react';
 import Editor from "./Editor";
 import Sale from "./Sale";
 import Loginphone from "./Loginphone";
 import { AnimateSharedLayout } from 'framer-motion';
+import { auth, getUserDocument } from './firebase';
 import { Route, Switch } from 'react-router';
 import useLocalStorage from './useLocalStorage';
+import { toast , ToastContainer } from "react-toastify";
 
 const SalaContext = createContext(null);
 export const useSala = () => useContext(SalaContext);
 
 function App() {
 
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    auth.onAuthStateChanged(async firebaseUser => {
+      if (firebaseUser) {
+        try {
+          const data = await getUserDocument(firebaseUser.uid);
+          setUser({ ...data });
+        } catch (error) {
+          toast.error(error.message); 
+        }
+      }
+      
+    });
+  }, []);
+
   const context = useLocalStorage('sala', '');
 
-  return (
-    <AnimateSharedLayout type="crossfade">
-      <SalaContext.Provider value={context}>
+  return user ? (
+    <SaleContext.Provider value={contex}>
+      <LoggedRouter />
+    </SaleContext.Provider>
+  ) : <NonLoggedRouter  /> ;
+  }
+
+ const LoggedRouter = () => (
+  <AnimateSharedLayout type="crossfade">
         <Switch>
           <Route path="/editor">
             <Editor />
@@ -27,10 +51,27 @@ function App() {
             <Sale />
           </Route>
         </Switch>
-      </SalaContext.Provider>
     </AnimateSharedLayout>
+);
 
-  );
-}
+const NonLoggedRouter = () => (
+  <AnimateSharedLayout type="crossfade">
+        <Switch>
+          <Route path="/">
+          <ToastContainer
+            position="top-right"
+            newestOnTop
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            pauseOnHover
+            draggable
+            hideProgressBar
+          />
+            <Loginphone />
+          </Route>
+        </Switch>
+    </AnimateSharedLayout>
+);
 
 export default App;
--- src/firebase.js
+++ src/firebase.js
@@ -45,3 +45,37 @@ export const sendVerificationCode = async (code) => {
     console.log({error})
   }
 }
+
+export const generateUserDocument = async (user, additionalData) => {
+  if (!user) return;
+  const userRef = firestore.doc(`users/${user.uid}`);
+  const snapshot = await userRef.get();
+  if (!snapshot.exists) {
+    const { email, displayName } = user;
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        ...additionalData
+      });
+    } catch (error) {
+      console.error("Error creating user document", error);
+    }
+  }
+  return getUserDocument(user.uid);
+};
+
+export const getUserDocument = async uid => {
+  if (!uid) return null;
+  try {
+    const userDocument = await firestore.doc(`users/${uid}`).get();
+    return {
+      uid,
+      ...userDocument.data()
+    };
+  } catch (error) {
+    console.error("Error fetching user", error);
+  }
+};
+export const auth = firebase.auth()
+export const firestore = firebase.firestore();
