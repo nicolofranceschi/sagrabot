dcb402a41df281d0d9ebc184f8006f951924e54c
Nicolofranceschi <franceschinicolo@gmail.com>
2021-07-14 17:30:46 +0200

    COMMIT

--- src/Choose/index.jsx
+++ src/Choose/index.jsx
@@ -1,40 +1,36 @@
 
 import { motion } from "framer-motion";
-import { Line, Container, Logo, LineMenu, Card, Grid } from "./styled";
+import { Line, Container, Logo, LineMenu, Card, CardMenu , Giorni , Mese , Immagine} from "./styled";
+import funghi from './funghi.jpeg'; 
 
-const motionDivProps = {
-    drag: 'x',
-    dragConstraints: {
-        left: '-100', right: '100'
-    }
-}
 
 export default function Choose() {
 
     const menu = [
         {
-            "nome": "manubimbo",
-            "prezzo": "100"
+            "nome": "Menu Adulti",
+            "prezzo": "200",
+            
         },
         {
-            "nome": "gigidag",
-            "prezzo": "100"
-        },
-        {
-            "nome": "zannabiancasuprema",
-            "prezzo": "100"
+            "nome": "MenuBimbi",
+            "prezzo": "100",
+            
         }
     ]
 
     const giorni = [
         {
-            "giorno": "20 agosto",
+            "giorno": "20",
         },
         {
-            "giorno": "27 agosto",
+            "giorno": "27",
         },
         {
-            "giorno": "29 agosto",
+            "giorno": "30",
+        },
+        {
+            "giorno": "29",
         },
     ]
 
@@ -46,31 +42,45 @@ export default function Choose() {
             "orario": "8",
         },
         {
-            "orario": "7",
+            "orario": "9",
         },
+        {
+            "orario": "10",
+        }
     ]
+
 
     return (
         <Container>
-            <Logo></Logo>
-            <motion.div {...motionDivProps}>
-                <LineMenu>
+            <Logo>SAGRABOT</Logo>
+            <motion.div drag="x" dragConstraints={{ left: -325, right: 0 }}>
+                <LineMenu >
                     {menu.map((a) => (
-                        <Card key={a.nome} ></Card>
+                        <CardMenu key={a.nome} >
+                            <Immagine src={funghi} />
+                            <p>{a.nome}</p>
+                            <p>{a.prezzo}</p>
+                        </CardMenu>
                     ))}
                 </LineMenu>
             </motion.div>
-            <motion.div {...motionDivProps}>
-                <Line>
+            <motion.div drag="x" dragConstraints={{ left: -450, right: 0 }}>
+                <Line > 
                     {giorni.map((b) => (
-                        <Card key={b.giorno} ></Card>
+                        <Card key={b.giorno} >
+                            <Giorni>{b.giorno}</Giorni>
+                            <Mese>AGOSTO</Mese>
+                        </Card>
                     ))}
                 </Line>
             </motion.div>
-            <motion.div {...motionDivProps}>
+            <motion.div drag="x" dragConstraints={{ left: -450, right: 0 }}>
                 <Line>
                     {orari.map((c) => (
-                        <Card key={c.orario}></Card>
+                        <Card key={c.orario}>
+                            <Giorni>{c.orario}</Giorni>
+                            <Mese>PM</Mese>
+                        </Card>
                     ))}
                 </Line>
             </motion.div>
--- src/Choose/styled.js
+++ src/Choose/styled.js
@@ -24,28 +24,77 @@ export const Grid = styled(motion.div)`
 
 export const Line = styled.div`
   height: 25vh;
-  width: 600px;
-  background-color: white;
+  width: 800px;
   display: flex;
 `;
+
 export const LineMenu = styled.div`
   height: 40vh;
-  width: 600px;
-  background-color: red;
+  width: 900px;
   display: flex;
 `;
 
 export const Logo = styled.div`
   height: 10vh;
   width: 100vw;
-  background-color: blue;
   overflow: hidden;
 `;
 
 export const Card = styled.div`
   height: 80%;
   width: 200px;
-  background-color: black;
+  background-color: white;
+  border-radius: 20px;
   margin: 20px;
+  :hover{
+      p{ 
+          color:pink;
+      }
+  }
+`;
+export const CardMenu = styled.div`
+  height: 80%;
+  width: 300px;
+  background-color: white;
+  border-radius: 20px;
+  margin: 20px;
+  p{
+      z-index: 1;
+      margin-top: -20px;
+      margin-bottom: 20px;
+      backdrop-filter: blur(10px);
+      border-radius: 20px;
+      margin-left:10px;
+      font-size: 30px;
+      font-family: Arial, Helvetica, sans-serif;
+  }
+  :hover{
+      p{ 
+          color:pink;
+      }
+  }
+`;
+
+export const Giorni = styled.p`
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 120px;
+  padding-left: 10px
+`;
+
+export const Mese = styled.p`
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 10px;
+  padding-left:10px;
+`;
+
+export const Immagine = styled.img`
+  overflow: hidden;
+  max-height:100%; 
+  max-width:100%;
+  border-radius: 20px;
+  position: center;
+  .blur {
+      filter: blur(2px);
+    }
 `;
 
